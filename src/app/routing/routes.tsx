d51895bbffbbd344e6cad2f6e75a2692e49c6b87
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-04-17 07:18:55 +0500

    Update routes.tsx

--- src/app/routing/routes.tsx
+++ src/app/routing/routes.tsx
@@ -1,5 +1,6 @@
 
 
+
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './../../context/ThemeContext';
 import { FontSizeProvider } from './../../context/FontContext';
