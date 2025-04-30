80550736d767b8fc4b8c7122c0ae3fc187df314c
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-04-30 22:17:56 +0500

    Update useTheme.ts

--- src/shared/hooks/useTheme.ts
+++ src/shared/hooks/useTheme.ts
@@ -9,6 +9,7 @@ import { useEffect } from 'react';
 
 
 
+
 export const useThemeToggle = () => {
 	const dispatch = useDispatch();
 	const { theme } = useSelector((state: RootState) => state.generel);
