b1502731ad1483bbd34d2d9d6ba42c7bab445791
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-05-10 00:01:55 +0500

    Update i18n.ts

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+
 import en from '../locales/en/common.json';
 import uz from '../locales/uz/common.json';
 import ru from '../locales/ru/common.json';
