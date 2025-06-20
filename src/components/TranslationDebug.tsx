487b08defd9c0e94aa307ee8c08cf1dca9b3a096
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-06-20 08:45:04 +0500

    Update TranslationDebug.tsx

--- src/components/TranslationDebug.tsx
+++ src/components/TranslationDebug.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next';
 export const TranslationDebug = () => {
   const { t, i18n } = useTranslation('common');
+
   
   return (
     <div style={{ display: 'none' }}>
